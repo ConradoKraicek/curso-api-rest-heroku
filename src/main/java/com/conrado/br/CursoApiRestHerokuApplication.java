677f5df0b76df1cebc3ca6e599b9c546d5e663ac
Conrado A. Cardozo <cacardoso@tjba.jus.br>
2021-02-08 09:33:01 -0300

    Projeto Criado

---
+++ src/main/java/com/conrado/br/CursoApiRestHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.conrado.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApiRestHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApiRestHerokuApplication.class, args);
+	}
+
+}
